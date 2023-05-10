8ecd56947eb7180237d7d909fa112a3404bfb15c
kotagiriramachandra <97685796+kotagiriramachandra@users.noreply.github.com>
2023-05-10 15:17:21 -0400

    Update git_build_scr.groovy

--- git_build_scr.groovy
+++ git_build_scr.groovy
@@ -2,7 +2,7 @@ def buildApp() {
             echo "Before ${params.BRANCH_NM}"
 	    checkout scmGit(branches: [[name: "*/${params.BRANCH_NM}"]], extensions: [], userRemoteConfigs: [[credentialsId: 'git-cred', name: 'origin', url: 'https://github.com/kotagiriramachandra/DevOpsClassCodes.git']])
             echo "After ${params.BRANCH_NM}"
-	    echo "Git pull from ${BRANCH_NM}"
+	    echo "Git pull from ${params.BRANCH_NM}"
 }
 
 def compileApp() {
